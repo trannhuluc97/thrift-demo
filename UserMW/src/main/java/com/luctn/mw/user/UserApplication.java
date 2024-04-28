fe3e1f1b7e8e650d0200170c0fbbd81bcd98c649
ngandtb2 <ngandtb2@vng.com.vn>
2024-04-28 11:20:13 +0700

    update

--- UserMW/src/main/java/com/luctn/mw/user/UserApplication.java
+++ UserMW/src/main/java/com/luctn/mw/user/UserApplication.java
@@ -4,13 +4,17 @@ import com.luctn.mw.user.server.TUserServer;
 import org.apache.thrift.server.TServer;
 import org.apache.thrift.transport.TTransportException;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UserApplication {
 
 	public static void main(String[] args) throws TTransportException {
-		SpringApplication.run(UserApplication.class, args);
+		SpringApplication application = new SpringApplication(UserApplication.class);
+		application.setWebApplicationType(WebApplicationType.NONE); //No RestController
+		application.run(args);
+
 		TUserServer server = new TUserServer();
 		server.start();
 	}
